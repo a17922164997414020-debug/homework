dc370eb587603d42dbedea898a726ff21b7cd587
a17922164997414020-debug <a17922164997414020@gmail.com>
2025-10-19 13:38:12 +0800

    Add files via upload

---
+++ 221/lab3/source/Main.c
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+
+int main(void) {
+   
+    printf("**********\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("**********\n");
+
+    printf("\n");
+
+   
+    printf("    ***\n");
+    printf("  *     *\n");
+    printf(" *       *\n");
+    printf(" *       *\n");
+    printf(" *       *\n");
+    printf(" *       *\n");
+    printf(" *       *\n");
+    printf("  *     *\n");
+    printf("    ***\n");
+   
+
+    printf("\n");
+
+    
+    printf("      *\n");
+    printf("     ***\n");
+    printf("    *****\n");
+    printf("      *\n");
+    printf("      *\n");
+    printf("      *\n");
+    printf("      *\n");
+    printf("      *\n");
+    printf("      *\n");
+    printf("\n");
+
+   
+    printf("      *\n");
+    printf("     * *\n");
+    printf("    *   *\n");
+    printf("   *     *\n");
+    printf("  *       *\n");
+    printf("   *     *\n");
+    printf("    *   *\n");
+    printf("     * *\n");
+    printf("      *\n");
+
+    printf("\n");
+
+
+    return 0;
+}
