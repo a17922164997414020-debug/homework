e9a838d12aadcfc2b9589213cf3ac6e3fe78aa79
a17922164997414020-debug <a17922164997414020@gmail.com>
2025-10-19 13:43:23 +0800

    Add files via upload

---
+++ 225/lab3/source/Main.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main(void) {
+   
+    printf("PPPPPPPPPP\n");
+    printf("    P    P\n");
+    printf("    P    P\n");
+    printf("    P    P\n");
+    printf("     P  P\n");
+    printf("\n");
+
+
+    printf("   J J\n");
+    printf("  J\n");
+    printf(" J\n");
+    printf("  J\n");
+    printf("   JJJJJJJ\n");
+    printf("\n");
+
+
+    printf("DDDDDDDDDD\n");
+    printf("D        D\n");
+    printf("D        D\n");
+    printf(" D      D\n");
+    printf("  DDDDD\n");
+
+    return 0;
+}
---
+++ 226/lab3/source/Main.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int num1, num2;
+
+    printf("Enter two integers: ");
+    scanf("%d%d", &num1, &num2);
+
+    if (num2 != 0 && num1 % num2 == 0) {
+        printf("%d is a multiple of %d\n", num1, num2);
+    }
+    else {
+        printf("%d is not a multiple of %d\n", num1, num2);
+    }
+
+    return 0;
+}
