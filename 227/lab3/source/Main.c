582ec3f49822024d63af055bc9b2f3f4eb007425
a17922164997414020-debug <a17922164997414020@gmail.com>
2025-10-19 13:44:35 +0800

    Add files via upload

---
+++ 227/lab3/source/Main.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+    printf("     *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    printf(" *********\n");
+
+    printf("\n");
+
+    printf("     *\n    ***\n   *****\n  *******\n *********\n");
+
+    return 0;
+}
