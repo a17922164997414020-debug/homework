ab875414cffe578bde67a2a2b288c9309864cf36
a17922164997414020-debug <a17922164997414020@gmail.com>
2025-10-19 13:40:52 +0800

    Add files via upload

---
+++ 223/lab3/source/Main.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int num1, num2, num3;
+    int largest, smallest;
+    printf("Enter three integers: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+    largest = num1;
+    if (num2 > largest) {
+        largest = num2;
+    }
+
+    if (num3 > largest) {
+        largest = num3;
+    }
+    smallest = num1;
+    if (num2 < smallest) {
+        smallest = num2;
+    }
+    if (num3 < smallest) {
+        smallest = num3;
+    }
+
+    printf("Largest integer is %d\n", largest);
+    printf("Smallest integer is %d\n", smallest);
+    system("pause");
+    return 0;
+}
