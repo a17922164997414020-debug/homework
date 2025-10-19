3774e534d317f5fb2de6fc57030c930f789d3a7a
a17922164997414020-debug <a17922164997414020@gmail.com>
2025-10-19 13:45:50 +0800

    Add files via upload

---
+++ 232/lab3/source/Main.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    float weightInPounds;
+    float heightInInches;
+    float bmi;
+    printf("Enter your weight in pounds: ");
+    scanf("%f", &weightInPounds);
+
+    printf("Enter your height in inches: ");
+    scanf("%f", &heightInInches);
+
+    bmi = (weightInPounds ) / (heightInInches * heightInInches);
+
+    printf("\nYour Body Mass Index (BMI) is: %.1f\n", bmi);
+    printf("\nBMI VALUES\n");
+    printf("Underweight: Less than 18.5\n");
+    printf("Normal:      between 18.5 and 24.9\n");
+    printf("Overweight:  between 25 and 29.9\n");
+    printf("Obese:       30 or greater\n");
+    printf("\nYour weight status based on integer BMI is: ");
+    if (bmi < 19) {
+        printf("Underweight\n");
+    }
+    else if (bmi >= 19 && bmi <= 24) {
+        printf("Normal\n");
+    }
+    else if (bmi >= 25 && bmi <= 29) {
+        printf("Overweight\n");
+    }
+    else { 
+        printf("Obese\n");
+    }
+
+    system("pause");
+    return 0;
+}
---
+++ 233/lab3/source/Main.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a, b, c, d, e, f, g;
+    printf("Total distance driven today (km): ");
+    scanf("%f", &a);
+    printf("Price of gasoline per liter: ");
+    scanf("%f", &b);
+    printf("Average kilometers per liter: ");
+    scanf("%f", &c);
+    printf("Parking fee for one day: ");
+    scanf("%f", &d);
+    printf("Toll fee for one day: ");
+    scanf("%f", &e);
+    f = (a / c) * b;  
+    g = f + d + e;     
+
+    printf("Total cost for one day = %.1f\n", g);
+
+    system("pause");
+    return 0;
+}
